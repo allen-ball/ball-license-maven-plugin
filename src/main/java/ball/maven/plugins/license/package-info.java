99c06d40daf7d737bb79ecb2c75f27e284a73892
Allen D. Ball <ball@hcf.dev>
2020-02-05 19:25:54 +0000

    Created from archetype.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball-license-maven-plugin/trunk@5293 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/maven/plugins/license/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * ball-license-maven-plugin {@link org.apache.maven.plugin.Mojo}s.
+ *
+ * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
+ * @version $Revision$
+ */
+package ball.maven.plugins.artifact;
