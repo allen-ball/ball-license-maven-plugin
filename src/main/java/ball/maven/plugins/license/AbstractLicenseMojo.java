31212fd72785eae019f97eccf871d1d1e2f458ff
Allen D. Ball <ball@hcf.dev>
2020-02-11 15:22:07 +0000

    Do not implement Contextualizable.  Removed unused @Parameter fields.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball-license-maven-plugin/trunk@5364 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/maven/plugins/license/AbstractLicenseMojo.java
+++ src/main/java/ball/maven/plugins/license/AbstractLicenseMojo.java
@@ -13,33 +13,19 @@ import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.FileTime;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.License;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.descriptor.PluginDescriptor;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.settings.Settings;
-import org.codehaus.plexus.PlexusConstants;
-import org.codehaus.plexus.PlexusContainer;
-import org.codehaus.plexus.context.Context;
-import org.codehaus.plexus.context.ContextException;
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.Contextualizable;
 
 import static lombok.AccessLevel.PROTECTED;
-import static org.apache.commons.lang3.StringUtils.defaultString;
-import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
 /**
  * Abstract base class for license {@link org.apache.maven.plugin.Mojo}s.
@@ -47,28 +33,8 @@ import static org.apache.commons.lang3.StringUtils.isNotEmpty;
  * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
  * @version $Revision$
  */
-@NoArgsConstructor @ToString @Slf4j
-public abstract class AbstractLicenseMojo extends AbstractMojo
-                                          implements Contextualizable {
-    protected PlexusContainer container = null;
-
-    @Parameter(required = true, readonly = true, defaultValue = "${session}")
-    @Getter
-    private MavenSession session = null;
-
-    @Parameter(required = true, readonly = true, defaultValue = "${settings}")
-    @Getter
-    private Settings settings = null;
-
-    @Parameter(required = true, readonly = true,
-               defaultValue = "${mojoExecution}")
-    @Getter
-    private MojoExecution mojo = null;
-
-    @Parameter(required = true, readonly = true, defaultValue = "${plugin}")
-    @Getter
-    private PluginDescriptor plugin = null;
-
+@NoArgsConstructor(access = PROTECTED) @ToString @Slf4j
+public abstract class AbstractLicenseMojo extends AbstractMojo {
     @Parameter(required = true, readonly = true, defaultValue = "${project}")
     @Getter
     private MavenProject project = null;
@@ -82,11 +48,6 @@ public abstract class AbstractLicenseMojo extends AbstractMojo
                property = "license.file", defaultValue = "${basedir}/LICENSE")
     @Getter
     private File file = null;
-
-    @Override
-    public void contextualize(Context context) throws ContextException {
-        container = (PlexusContainer) context.get(PlexusConstants.PLEXUS_KEY);
-    }
 
     protected void copy(URL from, Path to) throws MojoExecutionException,
                                                   MojoFailureException {
