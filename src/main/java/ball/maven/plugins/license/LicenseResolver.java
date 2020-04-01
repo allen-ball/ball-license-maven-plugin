3d2195d9cd96dd6ce6aa60464095e97d55e143f6
Allen D. Ball <ball@hcf.dev>
2020-04-01 00:07:10 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball-license-maven-plugin/trunk@5694 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/maven/plugins/license/LicenseResolver.java
@@ -0,0 +1,150 @@
+package ball.maven.plugins.license;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Stream;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+import org.spdx.rdfparser.license.AnyLicenseInfo;
+import org.spdx.rdfparser.license.ConjunctiveLicenseSet;
+import org.spdx.rdfparser.license.DisjunctiveLicenseSet;
+import org.spdx.rdfparser.license.ExtractedLicenseInfo;
+import org.spdx.rdfparser.license.License;
+import org.spdx.rdfparser.license.LicenseSet;
+import org.spdx.rdfparser.license.OrLaterOperator;
+import org.spdx.rdfparser.license.SpdxListedLicense;
+import org.spdx.rdfparser.license.WithExceptionOperator;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+
+/**
+ * {@link AnyLicenseInfo} resolver with methods to parse, look-up, and
+ * normalize.
+ *
+ * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
+ * @version $Revision$
+ */
+@Named @Singleton
+@Slf4j
+public class LicenseResolver {
+    /** @serial */ private final LicenseMap licenseMap;
+    /** @serial */ private final TextLicenseInfoParser textLicenseInfoParser;
+    /** @serial */ private final URLLicenseInfoParser urlLicenseInfoParser;
+
+    /**
+     * Sole constructor.
+     *
+     * @param   licenseMap      The injected {@link LicenseMap}.
+     * @param   urlLicenseInfoParser
+     *                          The injected {@link URLLicenseInfoParser}.
+     * @param   textLicenseInfoParser
+     *                          The injected {@link TextLicenseInfoParser}.
+     */
+    @Inject
+    public LicenseResolver(LicenseMap licenseMap,
+                           URLLicenseInfoParser urlLicenseInfoParser,
+                           TextLicenseInfoParser textLicenseInfoParser) {
+        this.licenseMap =
+            Objects.requireNonNull(licenseMap);
+        this.urlLicenseInfoParser =
+            Objects.requireNonNull(urlLicenseInfoParser);
+        this.textLicenseInfoParser =
+            Objects.requireNonNull(textLicenseInfoParser);
+    }
+
+    @PostConstruct
+    public void init() { }
+
+    @PreDestroy
+    public void destroy() { }
+
+    /**
+     * Method to parse {@link URLLicenseInfo} and
+     * {@link ExtractedLicenseInfo} ({@link TextLicenseInfo}) instances.
+     *
+     * @param   in            The {@link AnyLicenseInfo} to resolve.
+     *
+     * @return  Parsed {@link AnyLicenseInfo}.
+     */
+    public AnyLicenseInfo parse(AnyLicenseInfo in) {
+        AnyLicenseInfo out = in;
+
+        if (in instanceof URLLicenseInfo) {
+            out = urlLicenseInfoParser.parse(this, (URLLicenseInfo) in);
+        } else if (in instanceof ExtractedLicenseInfo) {
+            out = textLicenseInfoParser.parse(this, (ExtractedLicenseInfo) in);
+        }
+
+        return out;
+    }
+
+    /**
+     * Method to convert a {@link Collection} of {@link AnyLicenseInfo}
+     * instances to either a {@link DisjunctiveLicenseSet} (if {@code 0} or
+     * {@code 2} or more members) or a single {@link AnyLicenseInfo}.
+     *
+     * @param   in            The {@link Collection} of
+     *                        {@link AnyLicenseInfo} to process.
+     *
+     * @return  An {@link AnyLicenseInfo} representing the license(s).
+     */
+    public AnyLicenseInfo toLicense(Collection<AnyLicenseInfo> in) {
+        AnyLicenseInfo out = null;
+
+        if (in.size() == 1) {
+            out = in.iterator().next();
+        } else {
+            out =
+                new DisjunctiveLicenseSet(in.stream()
+                                          .toArray(AnyLicenseInfo[]::new));
+        }
+
+        return out;
+    }
+
+    /**
+     * Method to test if an {@link AnyLicenseInfo} is fully specified by
+     * SPDX license(s).
+     *
+     * @param   license         The {@link AnyLicenseInfo}.
+     *
+     * @return  {@code true} if fully specified; {@code false} otherwise.
+     */
+    public boolean isFullySpdxListed(AnyLicenseInfo license) {
+        boolean fullySpecified =
+             LicenseUtilityMethods.walk(license)
+            .filter(LicenseUtilityMethods::isLeaf)
+            .map(t -> (t instanceof License))
+            .reduce(Boolean::logicalAnd).orElse(false);
+
+        return fullySpecified;
+    }
+
+    /**
+     * Method to test if an {@link AnyLicenseInfo} is partially specified by
+     * SPDX license(s).
+     *
+     * @param   license         The {@link AnyLicenseInfo}.
+     *
+     * @return  {@code true} if partially specified; {@code false}
+     *          otherwise.
+     */
+    public boolean isPartiallySpdxListed(AnyLicenseInfo license) {
+        boolean partiallySpecified =
+            LicenseUtilityMethods.walk(license)
+            .filter(LicenseUtilityMethods::isLeaf)
+            .map(t -> (t instanceof License))
+            .reduce(Boolean::logicalOr).orElse(false);
+
+        return partiallySpecified;
+    }
+}
---
+++ src/main/java/ball/maven/plugins/license/LicenseUtilityMethods.java
@@ -0,0 +1,134 @@
+package ball.maven.plugins.license;
+
+import java.util.stream.Stream;
+import org.spdx.rdfparser.license.AnyLicenseInfo;
+import org.spdx.rdfparser.license.LicenseSet;
+import org.spdx.rdfparser.license.OrLaterOperator;
+import org.spdx.rdfparser.license.WithExceptionOperator;
+
+/**
+ * {@link AnyLicenseInfo} utility methods.
+ *
+ * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
+ * @version $Revision$
+ */
+public abstract class LicenseUtilityMethods {
+
+    /**
+     * Static method to create a {@link Stream} to walk the tree rooted at
+     * {@code root}.
+     *
+     * @param   root            The {@link AnyLicenseInfo} representing the
+     *                          root of the tree.
+     *
+     * @return  A {@link Stream}.
+     */
+    public static Stream<AnyLicenseInfo> walk(AnyLicenseInfo root) {
+        return Walker.<AnyLicenseInfo>walk(root,
+                                           LicenseUtilityMethods::childrenOf);
+    }
+
+    /**
+     * Static method to get the children of a {@link AnyLicenseInfo}.
+     *
+     * @param   node            The {@link AnyLicenseInfo}.
+     *
+     * @return  A possibly non-empty {@link AnyLicenseInfo AnyLicenseInfo[]}
+     *          for a {@link LicenseSet}, {@link OrLaterOperator},
+     *          or {@link WithExceptionOperator} node; an empty array
+     *          otherwise.
+     */
+    public static AnyLicenseInfo[] childrenOf(AnyLicenseInfo node) {
+        AnyLicenseInfo[] children = null;
+
+        if (node instanceof LicenseSet) {
+            children = ((LicenseSet) node).getMembers();
+        } else if (node instanceof OrLaterOperator) {
+            children =
+                new AnyLicenseInfo[] {
+                    ((OrLaterOperator) node).getLicense()
+                };
+        } else if (node instanceof WithExceptionOperator) {
+            children =
+                new AnyLicenseInfo[] {
+                    ((WithExceptionOperator) node).getLicense()
+                };
+        } else {
+            children = new AnyLicenseInfo[] { };
+        }
+
+        return children;
+    }
+
+    /**
+     * Returns the count of licenses represented by this
+     * {@link AnyLicenseInfo}: The number of members in a
+     * {@link LicenseSet} or {@code 1}.
+     *
+     * @param   license         The {@link AnyLicenseInfo}.
+     *
+     * @return  The license count.
+     */
+    public static int countOf(AnyLicenseInfo license) {
+        int count = 0;
+
+        if (license instanceof LicenseSet) {
+            count =
+                Stream.of(((LicenseSet) license).getMembers())
+                .mapToInt(t -> countOf(t))
+                .sum();
+        } else if (license != null) {
+            count = 1;
+        }
+
+        return count;
+    }
+
+    /**
+     * Static method to test if an {@link AnyLicenseInfo} is a branch.
+     *
+     * @param   node            The {@link AnyLicenseInfo}.
+     *
+     * @return  {@code true} if a branch; {@code false} otherwise.
+     */
+    public static boolean isBranch(AnyLicenseInfo node) {
+        return (isLicenseSet(node) || isOperator(node));
+    }
+
+    /**
+     * Static method to test if an {@link AnyLicenseInfo} is a leaf.
+     *
+     * @param   node            The {@link AnyLicenseInfo}.
+     *
+     * @return  {@code true} if a leaf; {@code false} otherwise.
+     */
+    public static boolean isLeaf(AnyLicenseInfo node) {
+        return (! isBranch(node));
+    }
+
+    /**
+     * Static method to test if an {@link AnyLicenseInfo}
+     * is a {@link LicenseSet}.
+     *
+     * @param   node            The {@link AnyLicenseInfo}.
+     *
+     * @return  {@code true} if a {@code node} is a {@link LicenseSet};
+     *          {@code false} otherwise.
+     */
+    public static boolean isLicenseSet(AnyLicenseInfo node) {
+        return (node instanceof LicenseSet);
+    }
+
+    /**
+     * Static method to test if an {@link AnyLicenseInfo} is an operator
+     * node ({@link OrLaterOperator} or {@link WithExceptionOperator}).
+     *
+     * @param   node            The {@link AnyLicenseInfo}.
+     *
+     * @return  {@code true} if an operator node; {@code false} otherwise.
+     */
+    public static boolean isOperator(AnyLicenseInfo node) {
+        return (node instanceof OrLaterOperator
+                || node instanceof WithExceptionOperator);
+    }
+}
